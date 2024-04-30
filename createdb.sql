466855a2f3fe12aad0e010862bc9337868a70db5
amonbofis <gabriel.zamy15@gmail.com>
2024-04-30 10:04:48 +0200

    start

--- createdb.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `cityswap` DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
-
-CREATE USER 'cityswap'@'%' IDENTIFIED BY 'cityswap';
-GRANT ALL PRIVILEGES ON `cityswap`.* TO 'cityswap'@'%';
-
-CREATE USER 'cityswap'@'localhost' IDENTIFIED BY 'cityswap';
-GRANT ALL PRIVILEGES ON `cityswap`.* TO 'cityswap'@'localhost';
